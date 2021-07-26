a77b7f21ec4c39e35ac01855ad703055c8a214f8
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-26 13:24:50 -0600

    commit tree 64ab07d6a8eb61eb9109220d49d9ef4899f9451e

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-const dodger = document.getElementById("dodger"); 
 
-function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const dodger = document.getElementById("dodger"); 
+
+// function moveDodgerLeft() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
-  } 
+//     if (left > 0) {
+//       dodger.style.left = `${left - 1}px`;
+//     }
+//   } 
 
-function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
+// function moveDodgerRight() {
+//     const leftNumbers = dodger.style.left.replace("px", "");
+//     const left = parseInt(leftNumbers, 10);
   
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
-    }
-  } 
+//     if (left > 0) {
+//       dodger.style.left = `${left + 1}px`;
+//     }
+//   } 
 
-document.addEventListener("keydown", function(e) {
-if (e.key === "ArrowLeft") {
-    moveDodgerLeft();
-}
-if (e.key === "ArrowRight") {
-    moveDodgerRight();
-}
+// document.addEventListener("keydown", function(e) {
+// if (e.key === "ArrowLeft") {
+//     moveDodgerLeft();
+// }
+// if (e.key === "ArrowRight") {
+//     moveDodgerRight();
+// }
 
-}); 
+// }); 
 
